7caa8d784400210e0c249c1b9dc1c808012d1027
irishcarbomb777 <danielclannan@gmail.com>
2023-04-19 14:04:32 -0400

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Thin Air</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.center}>
+          <Image
+            src="/thinair.svg"
+            alt="Thin Air Logo"
+            width={100}
+            height={100}
+            priority
+          />
+        </div>
+      </main>
+    </>
+  )
+}
